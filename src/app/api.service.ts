48f39d6de3645916fafc54bbe3e5b161dcf96ec4
FrancescoBorzi <borzifrancesco@gmail.com>
2018-12-02 20:31:57 +0100

    Use env for api baseUrl

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { PagePayload, PageResponse, PagesResponse } from './types';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  baseUrl = 'http://localhost:8080/';
+  baseUrl = environment.baseUrl;
   apiUrl = this.baseUrl + 'api/';
 
   constructor(protected httpClient: HttpClient) {}
